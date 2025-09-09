20bb41b79f3dad42a67aefc7e8c8b15ab02cc64e
judeSara <joudsara103@gmail.com>
2025-09-09 15:59:11 +0000

    Rectangle area

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
-
+double area = length * width;
+double perimeter = (length + width) * 2;
+cout << "Rectangle Properties:" << endl;
+cout << "length = " << length << endl;
+cout << "width = " << width << endl;
+cout << "area = " << area << endl;
+cout << "perimeter = " << perimeter << endl;
+return 0;
 }
